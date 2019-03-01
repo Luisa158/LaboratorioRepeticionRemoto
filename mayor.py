8d1a65e274d616697dde584f1f7a48bd5a0426ec
Monsalve Serrano <000401311@bga.upb>
2019-03-01 12:33:05 -0500

    mi primer commit

---
+++ mayor.py
@@ -0,0 +1,11 @@
+
+print("Programa de nuemro mayor")
+
+x=int(input("Introduzca un valor entero"))
+
+if x>0:
+    print("Valor mayor")
+else:
+    print("Valor negativo, menor a cero")
+
+print("Fin prgrama")
